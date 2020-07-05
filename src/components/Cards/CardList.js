e47f4550439a252a77ec120ac6112d33056378c9
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-05 16:35:43 -0700

    CardTemplate clean up

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -4,30 +4,46 @@ import styles from "./Cards.module.css";
 import CardTemplate from "./CardTemplate";
 
 const CardList = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  const dataArray = [
+    {
+      className: "infected",
+      title: "Active",
+      type: confirmed,
+      lastUpdate,
+      description: "Number of active cases of Covid 19",
+    },
+    {
+      className: "recovered",
+      title: "Recovered",
+      type: recovered,
+      lastUpdate,
+      description: "Number of recovered from Covid 19",
+    },
+    {
+      className: "deaths",
+      title: "Deaths",
+      type: deaths,
+      lastUpdate,
+      description: "Number of deaths caused by Covid 19",
+    },
+  ];
+
+  const renderContent = () => {
+    return dataArray.map((each) => (
+      <CardTemplate
+        className={each.className}
+        title={each.title}
+        type={each.type}
+        lastUpdate={each.lastUpdate}
+        description={each.description}
+      ></CardTemplate>
+    ));
+  };
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <CardTemplate
-          className="infected"
-          title="Active"
-          type={confirmed}
-          lastUpdate={lastUpdate}
-          description="Number of active cases of Covid 19"
-        ></CardTemplate>
-        <CardTemplate
-          className="recovered"
-          title="Recovered"
-          type={recovered}
-          lastUpdate={lastUpdate}
-          description="Number of recovered from Covid 19"
-        ></CardTemplate>
-        <CardTemplate
-          className="deaths"
-          title="Deaths"
-          type={deaths}
-          lastUpdate={lastUpdate}
-          description="Number of deaths caused by Covid 19"
-        ></CardTemplate>
+        {renderContent()}
       </Grid>
     </div>
   );
