11384d15370251879bc9a15890492312833e1751
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-05 10:53:39 -0700

    fix build issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Cards } from "./Cards/Cards";
+export { default as CardTemplate } from "./Cards/CardTemplate";
 export { default as Chart } from "./Chart/Chart";
 export { default as CountryPicker } from "./CountryPicker/CountryPicker";
